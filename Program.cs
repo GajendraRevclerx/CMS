cc06f320a03cce0cd8a1681bbdb01d94e0aa4e62
Santosh Sharma <santoshsharma.p48@gmail.com>
2026-03-31 17:46:39 +0530

    save changes

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
 builder.Services.AddSingleton<MongoDbContext>();
 builder.Services.AddSingleton<ComplaintService>();
+
+// Session State Configuration
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 // Authentication
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -40,10 +49,27 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
+
+// AUTH-SESSION CONSISTENCY CHECK
+// This forces logout if the user has an auth cookie but is missing their session (e.g. browser reopen)
+app.Use(async (context, next) =>
+{
+    if (context.User.Identity != null && context.User.Identity.IsAuthenticated)
+    {
+        if (string.IsNullOrEmpty(context.Session.GetString("AuthActive")))
+        {
+            await Microsoft.AspNetCore.Authentication.AuthenticationHttpContextExtensions.SignOutAsync(context, CookieAuthenticationDefaults.AuthenticationScheme);
+            context.Response.Redirect("/");
+            return;
+        }
+    }
+    await next();
+});
 
 app.MapStaticAssets();
 
